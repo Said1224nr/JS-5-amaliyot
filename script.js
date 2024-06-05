13ea0f02b2f41bdb8313d41d8ed9db0c3652684a
Said1224nr <162983493+Said1224nr@users.noreply.github.com>
2024-06-05 20:09:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let input = document.querySelector('input')
+let btn = document.querySelector('button')
+let img = document.querySelector('img')
+
+
+btn.addEventListener('click', () => {
+    img.setAttribute('src', input.value)
+})
